ade1a13aef5e47b941692a2ddd8a3aa1cec98708
libewa <67926131+libewa@users.noreply.github.com>
2023-08-09 15:38:35 +0200

    yes

--- Package.swift
+++ Package.swift
@@ -20,9 +20,5 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "bel",
-            dependencies: []),
-        .testTarget(
-            name: "belTests",
-            dependencies: ["bel"]),
-    ]
+            dependencies: []),    ]
 )
--- Sources/bel/bel.swift
+++ Sources/bel/bel.swift
@@ -1,6 +1,6 @@
 public struct bel {
-    public private(set) var text = "Hello, World!"
-
+    public var bel = String(Character(UnicodeScalar(8)))
     public init() {
+        print(bel)
     }
 }
