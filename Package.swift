b487e08616c30e4d126ca6222217360530ee6b30
libewa <67926131+libewa@users.noreply.github.com>
2023-08-09 15:54:26 +0200

    change from so to executable

--- Package.swift
+++ Package.swift
@@ -7,18 +7,13 @@ let package = Package(
     name: "bel",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "bel",
-            targets: ["bel"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .executable(name: "bel", targets: ["bel"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "bel",
-            dependencies: []),    ]
+            dependencies: []),
+    ]
 )
